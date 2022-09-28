bc9052018f37de3ec6214cdba566030f3c47e01f
Gabygamer007 <gabygamer0713@hotmail.com>
2022-09-28 14:25:07 -0400

    tp1

---
+++ Travaux/TP1/app/src/main/java/com/example/tp1/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.tp1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    static final String ClientID = "b036c77369924016a7370600b2e8e6fd";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

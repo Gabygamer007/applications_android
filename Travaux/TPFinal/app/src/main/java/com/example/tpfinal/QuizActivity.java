1ad1e10fbc12ce93beb78a6633b074d03edf6440
Gabygamer007 <gabygamer0713@hotmail.com>
2022-12-08 12:16:35 -0500

    tpfinal

--- Travaux/TPFinal/app/src/main/java/com/example/tpfinal/QuizActivity.java
+++ Travaux/TPFinal/app/src/main/java/com/example/tpfinal/QuizActivity.java
@@ -2,13 +2,30 @@ package com.example.tpfinal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class QuizActivity extends AppCompatActivity {
+    Button boutonQuiz;
+    ImageView imageTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+        boutonQuiz = findViewById(R.id.boutonQuiz);
+        imageTest = findViewById(R.id.imageTest);
+
+
+
+        boutonQuiz.setOnClickListener(view -> {
+            Intent intent = new Intent(QuizActivity.this, Question1Activity.class);
+            startActivity(intent);
+        });
+
+
     }
 }
